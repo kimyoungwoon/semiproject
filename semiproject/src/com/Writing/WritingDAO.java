1633fca05b4cc2c3c03ed0473e14a5382b15e415
ansooeon <tndjs963@gmail.com>
2022-08-02 16:38:57 +0900

    WritingDTO
    DTO

---
+++ semiproject/src/com/Writing/WritingDAO.java
@@ -0,0 +1,86 @@
+package com.Writing;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class WritingDAO {
+	
+	// 의존성주입
+	private Connection conn;
+
+	public WritingDAO(Connection conn) {
+		this.conn = conn;
+	}
+
+	// num의 최대값
+	public int getMaxNum() {
+
+		int maxNum = 0;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+
+		try {
+
+			sql = "select nvl(max(num),0) from writing ";
+
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				maxNum = rs.getInt(1);
+			}
+
+			rs.close();
+			pstmt.close();
+
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+
+		return maxNum;
+	}
+	
+	// 입력
+
+	public int insertData(WritingDTO dto) {
+
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql;
+
+		try {
+
+			sql = "insert into board (membernum,num,id,pw,name,email,subject,content,hitCount,created) ";
+			sql += "values (?,?,?,?,?,?,?,?,0,sysdate) ";
+
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setInt(1, dto.getMembernum());
+			pstmt.setInt(2, dto.getNum());
+			pstmt.setString(3, dto.getId());
+			pstmt.setString(4, dto.getPw());
+			pstmt.setString(5, dto.getName());
+			pstmt.setString(6, dto.getEmail());
+			pstmt.setString(7, dto.getSubject());
+			pstmt.setString(8, dto.getContent());
+
+			result = pstmt.executeUpdate();
+			pstmt.close();
+
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		return result;
+
+	}
+	
+	
+	
+
+}
---
+++ semiproject/src/com/Writing/WritingDTO.java
@@ -0,0 +1,78 @@
+package com.Writing;
+
+public class WritingDTO {
+	
+	private int membernum;
+	private int num;
+	private String id;
+	private String pw;
+	private String name;
+	private String email;
+	private String subject;
+	private String content;
+	private int hitcount;
+	private String created;
+	public int getMembernum() {
+		return membernum;
+	}
+	public void setMembernum(int membernum) {
+		this.membernum = membernum;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getHitcount() {
+		return hitcount;
+	}
+	public void setHitcount(int hitcount) {
+		this.hitcount = hitcount;
+	}
+	public String getCreated() {
+		return created;
+	}
+	public void setCreated(String created) {
+		this.created = created;
+	}
+	
+	
+
+}
