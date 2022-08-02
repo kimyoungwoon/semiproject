56acfde502097c4f64e746a8003d726b92e64681
youngjin3 <dudwlsdl515@naver.com>
2022-08-02 19:51:15 +0900

    Custominfo
    Custominfo class

---
+++ semiproject/src/com/member/Custominfo.java
@@ -0,0 +1,22 @@
+package com.member;
+
+public class Custominfo {
+
+	private String Id;
+	private String Name;
+	
+	public String getId() {
+		return Id;
+	}
+	public void setId(String Id) {
+		this.Id = Id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String Name) {
+		this.Name = Name;
+	}
+
+
+}
