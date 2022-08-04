6629b2b010b90de852c207c64824e98070c5e3b6
dongwoo0523 <dlehdxo2@gmail.com>
2022-08-04 20:54:10 +0900

    관리자페이지 추가
    제품등록 관리자페이지추가
    dao 조인테이블 값 넣을수있게추가
    뷰값 읽어와서 각 분류 추가할수있게
    업데이트

--- semiproject/src/com/shop/ProductDAO.java
+++ semiproject/src/com/shop/ProductDAO.java
@@ -72,18 +72,51 @@ public class ProductDAO {
 			pstmt.setInt(8, dto.getTag());
 			pstmt.setString(9, dto.getSaveFileName());
 			
+			
+			result = pstmt.executeUpdate();
+			/*
+			if(result==0) {
+				return result; //제품등록 실패
+			}
+			*/
+			
+			/*
+			//조인테이블에 제품사이즈 추가
+			sql = "insert into product_size (productnum,sizenum) ";
+			sql+= "values(?,?)";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, dto.getNum());
+			pstmt.setInt(2, dto.getPro_size());
 			result = pstmt.executeUpdate();
 			
+			//조인테이블에 제품컬러 추가
+			sql = "insert into product_color (productnum,COLORNUM) ";
+			sql+= "values(?,?)";
 			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, dto.getNum());
+			pstmt.setInt(2, dto.getColor());
+			result = pstmt.executeUpdate();
 			
+			//조인테이블에 제품컬러 추가
+			sql = "insert into product_tag (productnum,TAGNUM) ";
+			sql+= "values(?,?)";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, dto.getNum());
+			pstmt.setInt(2, dto.getTag());
+			result = pstmt.executeUpdate();
+			*/
 			pstmt.close();
 			
 		} catch (Exception e) {
 			System.out.println(e.toString());
+			return -1;//db오류 (제품등록 실패
 			 
 		}
 		
-		return result;
+		return result;//제품등록 성공
 		
 	}
 	
@@ -103,6 +136,46 @@ public class ProductDAO {
 			
 			pstmt.setInt(1, start);
 			pstmt.setInt(2, end);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				
+				dto = new ProductDTO();
+				
+				dto.setNum(rs.getInt("num"));
+				dto.setName(rs.getString("name"));
+				dto.setPrice(rs.getInt("price"));
+				dto.setCategory(rs.getInt("category"));
+				dto.setBrand(rs.getInt("brand"));
+				dto.setPro_size(rs.getInt("pro_size"));
+				dto.setColor(rs.getInt("color"));
+				dto.setTag(rs.getInt("tag"));
+				dto.setSaveFileName(rs.getString("saveFileName"));
+				
+				lists.add(dto);
+				
+			}
+			rs.close();
+			pstmt.close();
+			
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		
+		return lists;
+	}
+	
+	//페이징없는 관리자용 전체데이터 가져오기
+	public List<ProductDTO> product_getList(){
+		
+		lists = new ArrayList<ProductDTO>();
+		
+		try {
+			
+			sql = "select * from product order by num desc";
+			
+			pstmt = conn.prepareStatement(sql);
 			
 			rs = pstmt.executeQuery();
 			
@@ -202,8 +275,7 @@ public class ProductDAO {
 	
 	public int product_deleteDate(int num) {
 		int result = 0;
-		PreparedStatement pstmt = null;
-		String sql;
+		
 		
 		try {
 			
@@ -222,4 +294,76 @@ public class ProductDAO {
 		return result;
 	}
 	
+	//제품에 컬러 추가 (추가할 수량(count)을 받아서 한번에추가 
+	public int color_insertData(int num,int colorNum,int count) {
+		
+		int result = 0;
+		
+		try {
+			
+			for(int i=0;i<count;i++) {
+			sql = "insert into product_color (productnum,colornum) ";
+			sql+= "values (?,?)";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			pstmt.setInt(2, colorNum);
+			result = pstmt.executeUpdate();
+			pstmt.close();
+			}		
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		return result;
+	}
+	
+	//제품에 사이즈 추가 (추가할 수량(count)을 받아서 한번에추가 
+	public int size_insertData(int num,int sizeNum,int count) {
+		
+		int result = 0;
+		
+		try {
+			;
+			
+			for(int i=0;i<count;i++) {
+				sql = "insert into product_size (productnum,sizenum) ";
+				sql+= "values (?,?)";
+
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1, num);
+				pstmt.setInt(2, sizeNum);
+				result = pstmt.executeUpdate();
+				pstmt.close();
+				
+			}
+					
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		return result;
+	}
+	
+	//제품에 사이즈 추가 (추가할 수량(count)을 받아서 한번에추가
+	public int tag_insertData(int num,int tagNum,int count) {
+		
+		int result = 0;
+		
+		try {
+			
+			for(int i=0;i<count;i++) {
+			sql = "insert into product_tag (productnum,tagnum) ";
+			sql+= "values (?,?)";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			pstmt.setInt(2, tagNum);
+			result = pstmt.executeUpdate();
+			pstmt.close();
+			
+			}		
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		return result;
+	}
 }
