0def4348fbc6b5d32b4baa1b3757edcd2382df99
dongwoo0523 <dlehdxo2@gmail.com>
2022-08-05 11:22:59 +0900

    shop수정 dao추가
    사이즈별로 마라미터 받던거
    변수 하나로 다 처리하는걸로 수정
    
    관리자페이지 수정
    고객이 반복작업을 안할수도 있을거같아서 하나씩 입력하는걸로 수정
    
    카테고리별로 리스트반환
    listdao 추가
    클래스파일도 같이 작업할수있게 수정

--- semiproject/src/com/shop/ListReturnDAO.java
+++ semiproject/src/com/shop/ListReturnDAO.java
@@ -217,7 +217,149 @@ public class ListReturnDAO {
 			return lists;
 		}
 		
+		//사이즈별로 리스트 반환하는 메서드
+		public List<ProductDTO> size_getList(int start,int end,int size){
 
+			lists = new ArrayList<ProductDTO>();
+
+			try {
+				
+				sql =  "select * from (select rownum rnum,data.* from (";
+				sql+= "select e.num,e.name,e.price,e.category,e.brand,e.savefilename,";
+				sql+= "d.PRODUCTNUM,d.sizenum from PRODUCT E,PRODUCT_SIZE D ";
+				sql+= "WHERE E.NUM = D.PRODUCTNUM AND D.SIZENUM = ? ";
+				sql+= "order by num desc) data) where rnum >= ? and rnum <=?";
+
+				pstmt = conn.prepareStatement(sql);
+
+				pstmt.setInt(1, size);
+				pstmt.setInt(2, start);
+				pstmt.setInt(3, end);
+
+				rs = pstmt.executeQuery();
+
+				while(rs.next()) {
+
+					dto = new ProductDTO();
+
+					dto.setNum(rs.getInt("num"));
+					dto.setName(rs.getString("name"));
+					dto.setPrice(rs.getInt("price"));
+					dto.setCategory(rs.getInt("category"));
+					dto.setBrand(rs.getInt("brand"));
+					dto.setPro_size(rs.getInt("sizenum"));
+					dto.setSaveFileName(rs.getString("saveFileName"));
+
+					lists.add(dto);
+
+				}
+
+				rs.close();
+				pstmt.close();
+
+
+			} catch (Exception e) {
+				System.out.println(e.toString());
+			}
+
+			return lists;
+		}
+		//컬러카테고리별로 리스트 반환하는 메서드
+		public List<ProductDTO> color_getList(int start,int end,int color){
+
+			lists = new ArrayList<ProductDTO>();
+
+			try {
+
+				sql =  "select * from (select rownum rnum,data.* from (";
+				sql+= "select e.num,e.name,e.price,e.category,e.brand,e.savefilename,";
+				sql+= "d.PRODUCTNUM,d.colornum from PRODUCT E,PRODUCT_COLOR D ";
+				sql+= "WHERE E.NUM = D.PRODUCTNUM AND D.colorNUM = ? ";
+				sql+= "order by num desc) data) where rnum >= ? and rnum <=?";
+
+				pstmt = conn.prepareStatement(sql);
+
+				pstmt.setInt(1, color);
+				pstmt.setInt(2, start);
+				pstmt.setInt(3, end);
+
+				rs = pstmt.executeQuery();
+
+				while(rs.next()) {
+
+					dto = new ProductDTO();
+
+					dto.setNum(rs.getInt("num"));
+					dto.setName(rs.getString("name"));
+					dto.setPrice(rs.getInt("price"));
+					dto.setCategory(rs.getInt("category"));
+					dto.setBrand(rs.getInt("brand"));
+					dto.setColor(rs.getInt("colornum"));
+					dto.setSaveFileName(rs.getString("saveFileName"));
+
+					lists.add(dto);
+
+				}
+
+				rs.close();
+				pstmt.close();
+
+
+			} catch (Exception e) {
+				System.out.println(e.toString());
+			}
+
+			return lists;
+		}
+		//태그카테고리별로 리스트 반환하는 메서드
+		public List<ProductDTO> tag_getList(int start,int end,int tag){
+
+			lists = new ArrayList<ProductDTO>();
+
+			try {
+
+				sql =  "select * from (select rownum rnum,data.* from (";
+				sql+= "select e.num,e.name,e.price,e.category,e.brand,e.savefilename,";
+				sql+= "d.PRODUCTNUM,d.TAGNUM from PRODUCT E,PRODUCT_TAG D ";
+				sql+= "WHERE E.NUM = D.PRODUCTNUM AND D.TAGNUM = ? ";
+				sql+= "order by num desc) data) where rnum >= ? and rnum <=?";
+
+				pstmt = conn.prepareStatement(sql);
+
+				pstmt.setInt(1, tag);
+				pstmt.setInt(2, start);
+				pstmt.setInt(3, end);
+
+				rs = pstmt.executeQuery();
+
+				while(rs.next()) {
+
+					dto = new ProductDTO();
+
+					dto.setNum(rs.getInt("num"));
+					dto.setName(rs.getString("name"));
+					dto.setPrice(rs.getInt("price"));
+					dto.setCategory(rs.getInt("category"));
+					dto.setBrand(rs.getInt("brand"));
+					dto.setColor(rs.getInt("tagnum"));
+					dto.setSaveFileName(rs.getString("saveFileName"));
+
+					lists.add(dto);
+
+				}
+
+				rs.close();
+				pstmt.close();
+
+
+			} catch (Exception e) {
+				System.out.println(e.toString());
+			}
+
+			return lists;
+		}
+		
+		
 		
 }
 
